fceab574da2dacca656ec8d4433ae36f5fd2494c
Farzana Bente Alam <farzana.alam.aiub@gmail.com>
2026-06-23 22:51:10 -0700

    File updated

---
+++ HelloWorld/main.c
@@ -0,0 +1,12 @@
+// Write a program that prints your Name, ID and Department in three separate lines.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Farzana Bente Alam\n");
+    printf("Computer Science Dept\n");
+    printf("1809-****-2 \n");
+    return 0;
+}
